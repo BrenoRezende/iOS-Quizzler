353576a4815b12940a0ffa70cc662dd8b29dd9ca
Breno Rezende <brenorrezende@gmail.com>
2019-07-23 09:42:34 -0300

    First commit

---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Breno Rezende on 23/07/2019.
+//  Copyright (c) 2019 Brezende. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //Place your instance variables here
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var progressBar: UIView!
+    @IBOutlet weak var progressLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func answerPressed(_ sender: AnyObject) {
+  
+    }
+    
+    func updateUI() {
+      
+    }
+
+    func nextQuestion() {
+        
+    }
+    
+    func checkAnswer() {
+        
+    }
+    
+    func startOver() {
+       
+    }
+    
+}
